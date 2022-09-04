11854a2e70169fc19060e646dc02c0ba35513549
Iliyana Stoyanova <iliyana.t.stoyanova@gmail.com>
2022-09-04 05:52:55 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const fName = "Iliyana";
-const lName = "Stoyanova";
-const luckNum = 7
+const name = "Iliyana";
+const currentDate = new Date().getFullYear();
 ReactDOM.render(
-  <div>
-    <h1>Hello {`${fName} ${lName}`}</h1>
-    <p>Your lucky number is {luckNum}</p>
-  </div>,
-  document.getElementById("root")
+    <div>
+        <p>Created by {name}</p>
+        <p>Copyright {currentDate}</p>
+    </div>,
+    document.getElementById("root")
 );
