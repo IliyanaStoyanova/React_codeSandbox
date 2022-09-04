61a1eddff868cd467ebda2d7fe171894351f79b8
Iliyana Stoyanova <iliyana.t.stoyanova@gmail.com>
2022-09-04 07:30:40 +0000

    Update 3 files

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Heading() {
-    return <h1>Hello World!</h1>
+    const date = new Date();
+    const currentTime = date.getHours();
+
+    let greeting = "Good ";
+    let customStyle = {
+        color: ""
+    }
+    if(currentTime < 12) {
+        greeting += "Morning";
+        customStyle.color = "red";
+    }else if(currentTime < 18) {
+        greeting += "Afternoon";
+        customStyle.color = "green";
+    }else {
+        greeting += "Evening";
+        customStyle.color = "blue";
+    }
+    return (
+        <h1 className="heading" style={customStyle}>{greeting}</h1>
+    )
 }
 
 export default Heading;
