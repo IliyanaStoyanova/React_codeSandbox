bec8ca06c6e1e98cd69107b2b11680ca554b5465
Iliyana Stoyanova <iliyana.t.stoyanova@gmail.com>
2022-09-04 06:28:14 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const img = "https://picsum.photos/200";
+const customStyle = {
+    color: "red",
+    fontSize: "20px",
+    border: "1px solid black",
+};
+customStyle.color = "blue";
 ReactDOM.render(
     <div>
-        <h1 className="heading">My Favourite Foods</h1>
+        <h1 style={customStyle}>My Favourite Foods</h1>
         <div>
             <img alt="random" src={img + "?grayscale"} />
             <img alt="potatoes" className="food-img" src="https://www.seriouseats.com/thmb/U4eRN44zNTpRDRG2W8DBMQH626s=/880x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20211201-crispy-roasted-potatoes-vicky-wasik-45-d75608ce325e4ffbab665084eba642c8.jpg" />
