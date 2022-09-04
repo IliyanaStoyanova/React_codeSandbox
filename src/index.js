7b52c266b0eb9ac9feb666edfe45dc019f4edd8c
Iliyana Stoyanova <iliyana.t.stoyanova@gmail.com>
2022-09-04 05:40:44 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "Iliyana";
+const lName = "Stoyanova";
+const luckNum = 7
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1>Hello {`${fName} ${lName}`}</h1>
+    <p>Your lucky number is {luckNum}</p>
   </div>,
   document.getElementById("root")
 );
