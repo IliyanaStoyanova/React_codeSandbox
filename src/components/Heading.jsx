dd0903b7bf4d8544cdb6b843d8ba3120db60f8ab
Iliyana Stoyanova <iliyana.t.stoyanova@gmail.com>
2022-09-05 20:39:58 +0000

    Update 3 files

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -22,5 +22,4 @@ function Heading() {
         <h1 className="heading" style={customStyle}>{greeting}</h1>
     )
 }
-
 export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+import {add, multiply, subtract, divide} from "./calcolator";
 ReactDOM.render(
-    <App />,
+    // <App />,
+    <div>
+        {add(2, 3)},
+        {multiply(2, 3)},
+        {subtract(6, 3)},
+        {divide(6, 2)}
+    </div>,
     document.getElementById("root")
 );
